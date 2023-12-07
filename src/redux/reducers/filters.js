19dce78fb25f37d8ea550c2347dd3de6f6fd9710
Murat <renja.dizz@gmail.com>
2023-12-07 15:03:01 +0900

    start with filters checked

--- src/redux/reducers/filters.js
+++ src/redux/reducers/filters.js
@@ -1,9 +1,9 @@
 const initialState = [
-  { id: 'filter_all', name: 'Все', checked: false },
-  { id: 'filter_without', amount: 0, name: 'Без пересадок', checked: false },
-  { id: 'filter_one', amount: 1, name: '1 пересадка', checked: false },
-  { id: 'filter_two', amount: 2, name: '2 пересадки', checked: false },
-  { id: 'filter_three', amount: 3, name: '3 пересадки', checked: false },
+  { id: 'filter_all', name: 'Все', checked: true },
+  { id: 'filter_without', amount: 0, name: 'Без пересадок', checked: true },
+  { id: 'filter_one', amount: 1, name: '1 пересадка', checked: true },
+  { id: 'filter_two', amount: 2, name: '2 пересадки', checked: true },
+  { id: 'filter_three', amount: 3, name: '3 пересадки', checked: true },
 ];
 
 function filters(state = initialState, action) {
